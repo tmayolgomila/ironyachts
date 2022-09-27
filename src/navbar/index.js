cad82b4e9c5819d4356eff1455d67dbe9cb4989d
Tomeu Mayol <tmayolgomila@gmail.com>
2022-09-27 13:35:58 +0200

    desde desarrollo

---
+++ src/navbar/index.js
@@ -0,0 +1,18 @@
+import './styles.css';
+
+export default function Navbar(){
+    return(
+        <div className='containerNav'>
+            <a className='titulo'>BiblioYacht</a>
+            <nav>
+            <ul>
+            <li><a>home</a></li>
+            <li><a>archivos</a></li>
+            <li><a>archivos</a></li>
+            <li><a>archivos</a></li>
+            <li><a>archivos</a></li>
+            </ul>
+            </nav>
+        </div>
+    )
+}
