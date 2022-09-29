468bce776aeea1ee8aa6a292750f66b9f2190936
Tomeu Mayol <tmayolgomila@gmail.com>
2022-09-29 20:08:34 +0200

    coloco loading

--- src/App.js
+++ src/App.js
@@ -10,13 +10,8 @@ export default function App() {
   return (
     
     <div className="App">
-      <Suspense fallback={<div className="wrapper">
-    <div class="circle"></div>
-    <div class="circle"></div>
-    <div class="circle"></div>
-    <div class="shadow"></div>
-    <div class="shadow"></div>
-    <div class="shadow"></div>
+      <Suspense fallback={<div class="loader">
+    <div class="waves"></div>
 </div>} >
       <BrowserRouter basename={basename}>
     <Navbar/>
